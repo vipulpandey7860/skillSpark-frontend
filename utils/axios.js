fa2424541620da4c03bf95d26d41b9d7c16aa09b
i-vipulpandey <vipulpandey7860@gmail.com>
2023-09-21 23:32:30 +0530

    .

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "https://internshala-backend.onrender.com/",
     withCredentials: true,
 });
 
